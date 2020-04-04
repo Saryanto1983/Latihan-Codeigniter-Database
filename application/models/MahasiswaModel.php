aed7350bd0ae04ca723d7aa80078df48da35ffa5
Saryanto1983 <yanclp83@gmail.com>
2020-04-04 14:51:59 +0700

    update_data

--- application/models/MahasiswaModel.php
+++ application/models/MahasiswaModel.php
@@ -10,13 +10,32 @@
             parent::__construct();
         }
 
+        // input data (insert)
         function insert($data) 
         {
             $this->db->insert($this->table, $data);
         }
 
+        //menampilkan data 
         function tampil_data() 
         {
             return $this->db->get('mahasiswa');
+        }
+
+        //menampilkan data mana yang akan diedit/update
+        function edit_data($where, $table) {
+            return $this->db->get_where($table, $where);
+        }
+
+        //proses update datanya
+        function update_data($where, $data, $table) {
+            $this->db->where($where);
+            $this->db->update($table, $data);
+        }
+
+        //proses hapus data mahasiswa
+        function hapus_data($where, $table) {
+            $this->db->where($where);
+            $this->db->delete($table, $where);
         }
     }
